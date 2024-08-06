7357248d248b4316585fca5bf8a7941ebceb579f
Sandin Kodagoda <sandinkodagoda@gmail.com>
2024-08-06 09:51:25 +0530

    hello

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// HomePage.js
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./Login.scss"; // Ensure this file includes the necessary styles
+import logo from "../Assest/logo.png";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container screen-1">
+      <img src={logo} alt="Logo" className="login-image" />{" "}
+      {/* Add your image here */}
+      <form onSubmit={handleSubmit}>
+        <div className="input-group email">
+          <i className="icon-mail"></i> {/* Mail icon */}
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="input-group password">
+          <i className="icon-lock"></i> {/* Lock icon */}
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <button type="submit" className="login">
+          Login
+        </button>
+      </form>
+      <div className="footer">
+        <span>Forgot Password?</span>
+        <span>Sign Up</span>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
