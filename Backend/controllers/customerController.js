039751bde235422c389173b83cebe6c886679cc8
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-08-20 15:10:53 +0530

    updated

--- Backend/controllers/customerController.js
+++ Backend/controllers/customerController.js
@@ -5,7 +5,17 @@ const customersCollection = db.collection("customers");
 
 // Create a new customer
 export const createCustomer = async (req, res) => {
-  const { name, email, contactNumber, purchaseHistory } = req.body;
+  const {
+    surName,
+    name,
+    email,
+    contactNumber,
+    HouseNo,
+    street,
+    city,
+    postCode,
+    purchaseHistory,
+  } = req.body;
 
   try {
     // Check if a product with the same productID or name already exists
@@ -39,7 +49,27 @@ export const createCustomer = async (req, res) => {
         .send({ message: "Customer with this Contact Number already exists." });
     }
 
-    const customer = new Customer(name, email, contactNumber, purchaseHistory);
+    const existingCustomerHomeNoSnapshot = await customersCollection
+      .where("HouseNo", "==", HouseNo)
+      .get();
+
+    if (!existingCustomerHomeNoSnapshot.empty) {
+      return res
+        .status(400)
+        .send({ message: "Customer with this HouseNo already exists." });
+    }
+
+    const customer = new Customer(
+      surName,
+      name,
+      email,
+      contactNumber,
+      HouseNo,
+      street,
+      city,
+      postCode,
+      purchaseHistory
+    );
 
     const docRef = await customersCollection.add({ ...customer });
     res
