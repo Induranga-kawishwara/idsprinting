137191561d39ebf20ed009c88a2d3474837d1edd
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-08-31 04:17:00 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,23 +1,13 @@
-// import admin from "firebase-admin";
-// import { FIREBASE_ADMIN_SDK_KEY } from "./config.js";
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(FIREBASE_ADMIN_SDK_KEY),
-//   projectId: "ids-printing",
-// });
-
-// const db = admin.firestore();
-// export default db;
-
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-dotenv.config();
+
+dotenv.config(); // Load environment variables from .env file
 
 const serviceAccount = {
   type: process.env.type,
   project_id: process.env.project_id,
   private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"), // Replacing literal \n with actual newline
   client_email: process.env.client_email,
   client_id: process.env.client_id,
   auth_uri: process.env.auth_uri,
