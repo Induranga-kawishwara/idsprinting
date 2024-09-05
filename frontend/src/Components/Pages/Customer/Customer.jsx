eb40f4c9b8b22c8bda4c9a16a758994fdb3fed93
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-09-05 20:51:07 +0530

    Update Customer.jsx

--- frontend/src/Components/Pages/Customer/Customer.jsx
+++ frontend/src/Components/Pages/Customer/Customer.jsx
@@ -10,7 +10,7 @@ import CustomerFormModal from "./CustomerFormModal"; // Adjust the import path
 import { io } from "socket.io-client";
 
 // Initialize the socket connection
-const socket = io("http://localhost:8080");
+const socket = io("https://idsprinting.vercel.app");
 
 const Customer = () => {
   const [customers, setCustomers] = useState([]);
