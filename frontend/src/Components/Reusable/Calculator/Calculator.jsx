5f0e12ab98ff3f661813d8c66b5e7eade1edaca6
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-09-12 15:05:06 +0530

    updated calculator

--- frontend/src/Components/Reusable/Calculator/Calculator.jsx
+++ frontend/src/Components/Reusable/Calculator/Calculator.jsx
@@ -1,6 +1,7 @@
-import "./Calculator.scss";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { evaluate } from "mathjs";
+import Draggable from "react-draggable";
+import "./Calculator.scss";
 
 const Calculator = ({ onClose }) => {
   const [result, setResult] = useState("");
@@ -25,23 +26,26 @@ const Calculator = ({ onClose }) => {
     }
   };
 
-  const handleKeyPress = (e) => {
-    const { key } = e;
+  const handleKeyPress = useCallback(
+    (e) => {
+      const { key } = e;
 
-    if (
-      (/\d/.test(key) || ["+", "-", "*", "/", ".", "(", ")"].includes(key)) &&
-      result !== "Error"
-    ) {
-      setResult(result.concat(key));
-    } else if (key === "Enter") {
-      e.preventDefault(); // Prevent form submission
-      calculate();
-    } else if (key === "Backspace") {
-      handleDelete();
-    } else if (key === "Escape") {
-      clear();
-    }
-  };
+      if (
+        (/\d/.test(key) || ["+", "-", "*", "/", ".", "(", ")"].includes(key)) &&
+        result !== "Error"
+      ) {
+        setResult(result.concat(key));
+      } else if (key === "Enter") {
+        e.preventDefault(); // Prevent form submission
+        calculate();
+      } else if (key === "Backspace") {
+        handleDelete();
+      } else if (key === "Escape") {
+        clear();
+      }
+    },
+    [result]
+  );
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
@@ -56,87 +60,89 @@ const Calculator = ({ onClose }) => {
   };
 
   return (
-    <div className="Calculator">
-      <div className="Calculator-header">
-        <h1>Calculator</h1>
-        <button className="close-button" onClick={onClose}>
-          ×
-        </button>
-      </div>
-      <div className="container">
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={result} readOnly />
-        </form>
-
-        <div className="keypad">
-          <button onClick={clear} id="clear" className="highlight">
-            AC
-          </button>
-          <button onClick={handleDelete} className="highlight">
-            DEL
-          </button>
-          <button name="(" onClick={handleClick} className="highlight">
-            (
-          </button>
-          <button name=")" onClick={handleClick} className="highlight">
-            )
-          </button>
-
-          <button name="7" onClick={handleClick}>
-            7
-          </button>
-          <button name="8" onClick={handleClick}>
-            8
-          </button>
-
-          <button name="9" onClick={handleClick}>
-            9
-          </button>
-          <button name="/" onClick={handleClick} className="highlight">
-            &divide;
-          </button>
-          <button name="4" onClick={handleClick}>
-            4
-          </button>
-          <button name="5" onClick={handleClick}>
-            5
-          </button>
-          <button name="6" onClick={handleClick}>
-            6
-          </button>
-          <button name="*" onClick={handleClick} className="highlight">
-            &times;
-          </button>
-
-          <button name="1" onClick={handleClick}>
-            1
-          </button>
-          <button name="2" onClick={handleClick}>
-            2
-          </button>
-          <button name="3" onClick={handleClick}>
-            3
-          </button>
-          <button name="-" onClick={handleClick} className="highlight">
-            -
-          </button>
-
-          <button name="0" onClick={handleClick}>
-            0
-          </button>
-          <button name="." onClick={handleClick}>
-            .
-          </button>
-          <button></button>
-          <button name="+" onClick={handleClick} className="highlight">
-            +
-          </button>
-          <button onClick={calculate} id="equal" className="highlight">
-            =
+    <Draggable>
+      <div className="Calculator">
+        <div className="Calculator-header">
+          <h1>Calculator</h1>
+          <button className="close-button" onClick={onClose}>
+            ×
           </button>
         </div>
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={result} readOnly />
+          </form>
+
+          <div className="keypad">
+            <button onClick={clear} id="clear" className="highlight">
+              AC
+            </button>
+            <button onClick={handleDelete} className="highlight">
+              DEL
+            </button>
+            <button name="(" onClick={handleClick} className="highlight">
+              (
+            </button>
+            <button name=")" onClick={handleClick} className="highlight">
+              )
+            </button>
+
+            <button name="7" onClick={handleClick}>
+              7
+            </button>
+            <button name="8" onClick={handleClick}>
+              8
+            </button>
+
+            <button name="9" onClick={handleClick}>
+              9
+            </button>
+            <button name="/" onClick={handleClick} className="highlight">
+              &divide;
+            </button>
+            <button name="4" onClick={handleClick}>
+              4
+            </button>
+            <button name="5" onClick={handleClick}>
+              5
+            </button>
+            <button name="6" onClick={handleClick}>
+              6
+            </button>
+            <button name="*" onClick={handleClick} className="highlight">
+              &times;
+            </button>
+
+            <button name="1" onClick={handleClick}>
+              1
+            </button>
+            <button name="2" onClick={handleClick}>
+              2
+            </button>
+            <button name="3" onClick={handleClick}>
+              3
+            </button>
+            <button name="-" onClick={handleClick} className="highlight">
+              -
+            </button>
+
+            <button name="0" onClick={handleClick}>
+              0
+            </button>
+            <button name="." onClick={handleClick}>
+              .
+            </button>
+            <button></button>
+            <button name="+" onClick={handleClick} className="highlight">
+              +
+            </button>
+            <button onClick={calculate} id="equal" className="highlight">
+              =
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </Draggable>
   );
 };
 
