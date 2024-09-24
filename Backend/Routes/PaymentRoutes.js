6dd29ad09700a3bb9b4705aa54b95e242bb7a412
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2024-09-24 14:30:50 +0530

    updated

--- Backend/Routes/PaymentRoutes.js
+++ Backend/Routes/PaymentRoutes.js
@@ -4,7 +4,6 @@ import {
   getCustomerAndPaymentDetails,
   updatePaymentByPaymentId,
   deletePaymentByPaymentId,
-  deleteAndUpdate,
 } from "../Controllers/PaymentController.js";
 
 const router = express.Router();
@@ -12,10 +11,6 @@ const router = express.Router();
 router.post("/payment/:customerId", createPayment);
 router.get("/payment/:customerId/:paymentId", getCustomerAndPaymentDetails);
 router.put("/payment/:customerId/:paymentId", updatePaymentByPaymentId);
-router.put(
-  "/payment/:prevcustomerId/:newcustomerId/:paymentId",
-  deleteAndUpdate
-);
 router.delete("/payment/:customerId/:paymentId", deletePaymentByPaymentId);
 
 export default router;
